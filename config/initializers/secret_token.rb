be948b5bf8c957dd0d3cd77a3fdd477177d49220
Viacheslav Okolitiy <okolitiy.viacheslav@yandex.ua>
2014-10-15 00:21:39 +0300

    New

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Enyo::Application.config.secret_key_base = 'ee8a1e33a21fd23551c2fef9cd2e52a10d114e8e93c187a23488c807be99bd27836660221e5d250caaec53f57202e636d83bd9cd5417c9a30f74bd7d66e34d08'
